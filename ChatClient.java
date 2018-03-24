132422b6acecec3732f73d1ecf91e3c4b69232b1
revathi <revathits001@gmail.com>
2018-03-24 11:38:24 +0530

    Chatclient updated

--- ChatClient.java
+++ ChatClient.java
@@ -15,3 +15,11 @@ class ChatClient
       String sentence = inFromUser.readLine();
       sendData = sentence.getBytes();
       DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+	 clientSocket.send(sendPacket);
+      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+      clientSocket.receive(receivePacket);
+      String modifiedSentence = new String(receivePacket.getData());
+      System.out.println("FROM SERVER:" + modifiedSentence);
+      clientSocket.close();
+   }
+}
